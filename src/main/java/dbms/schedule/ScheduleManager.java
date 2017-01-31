f85cc943f29d3344bc44640813d15d4c36a52b72
Ivan <acccko@gmail.com>
2017-01-31 07:58:02 +0300

    add singlethons for managers

--- src/main/java/dbms/schedule/ScheduleManager.java
+++ src/main/java/dbms/schedule/ScheduleManager.java
@@ -1,4 +1,9 @@
 package dbms.schedule;
 
 public class ScheduleManager {
+    public static final ScheduleManager instance = new ScheduleManager();
+
+    public ScheduleManager getInstance() {
+        return instance;
+    }
 }
