d4b505f2402a78aaac469fbb41ea3362bf2c4d3d
Ivan <acccko@gmail.com>
2017-01-31 09:18:46 +0300

    add some code

--- src/main/java/dbms/storage/BufferManager.java
+++ src/main/java/dbms/storage/BufferManager.java
@@ -3,8 +3,12 @@ package dbms.storage;
 import dbms.query.QueryPlan;
 import dbms.query.QueryResult;
 
+import java.util.HashMap;
+
 public class BufferManager {
     public static final BufferManager instance = new BufferManager();
+    private HashMap<String, Page> bufferTable = new HashMap<String, Page>();
+    private DiskManager diskManager = DiskManager.getInstance();
 
     public BufferManager getInstance() {
         return instance;
@@ -14,5 +18,14 @@ public class BufferManager {
         QueryResult queryResult = new QueryResult();
         //TODO write inteaction with DiskManager and inner translation table
         return queryResult;
+    }
+
+    public boolean isBuffered(String pageId) {
+        return this.bufferTable.containsKey(pageId);
+    }
+
+    public void bufferPage(String pageId) {
+        // TODO load string
+        this.bufferTable.put(pageId, diskManager.getPage(pageId));
     }
 }
--- src/main/java/dbms/storage/DiskManager.java
+++ src/main/java/dbms/storage/DiskManager.java
@@ -3,7 +3,12 @@ package dbms.storage;
 public class DiskManager {
     public static final DiskManager instance = new DiskManager();
 
-    public DiskManager getInstance() {
+    public static DiskManager getInstance() {
         return instance;
+    }
+
+    public Page getPage(String pageId) {
+        Page page = new Page();
+        return page;
     }
 }
